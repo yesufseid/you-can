3b6f9700397d3a3a7ad7f7a450aaa94d1a844678
Seid Yesuf <seidyesuf750@gmail.com>
2025-04-29 10:55:11 +0300

    build

--- app/components/VolumeCalculater.tsx
+++ app/components/VolumeCalculater.tsx
@@ -1,23 +1,11 @@
-// src/pages/index.tsx or wherever you need the category manager
-import React, { useState } from 'react';
+
 import CategoryManager from './CategoryManager';
-import { calculateWeight } from '../../utils/calculateWeight';
+
 
 const MainPage: React.FC = () => {
-  const [selectedCategory, setSelectedCategory] = useState<any>(null);
-  const [volume, setVolume] = useState(0);
-  const [weight, setWeight] = useState<number | null>(null);
-
-  const handleCategorySelect = (category: any) => {
-    setSelectedCategory(category);
-  };
-
- 
-
   return (
     <div>
-      <CategoryManager onCategorySelect={handleCategorySelect} />
-
+      <CategoryManager  />
     </div>
   );
 };
