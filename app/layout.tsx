9bcbe4a6a2fe2e91dddd24c81b0b94feb9e20039
Seid Yesuf <seidyesuf750@gmail.com>
2025-05-04 13:27:36 +0300

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "You-Can Measure",
+  description: "A simple tool to help households estimate the weight of their plastic waste by item count or image, built with Next.js and Supabase.",
 };
 
 const geistSans = Geist({
